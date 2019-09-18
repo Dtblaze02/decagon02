60aedb1d23efbdbb8db3b3c0eeab868815bfa580
Dtblaze02 <sadiqaliyu89@gmail.com>
2019-09-18 12:41:53 +0100

    modified files

--- js/signup.js
+++ js/signup.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
+  //Registration Function
+  $('.regSubmitBtn').click(function(event) {
     event.preventDefault();
-    const hotelname = $('#hotelname').val();
+    const fullname = $('#fullname').val();
+    const username = $('#username').val();
+    const password = $('#password').val();
     const email = $('#email').val();
-    const mobile = $('#mobile').val();
-    const website = $('#website').val();
-    const location = $('#location').val();
-    const city = $('#city').val();
-    const address = $('#address').val();
-    
     //Check if user input is empty
-    if (!hotelname || !email || !mobile || !website || !location || !city || !address) {
+    if (!fullname || !username || !password || !email) {
       $('.regMessage').html('Kindly fill in all fields');
       return;
     }
     //Make get request to check if the user already exist
     $.ajax({
       method: 'GET',
-      url: `http://localhost:3000/hotels?email=${email}`,
+      url: `http://localhost:3000/users?email=${email}`,
       data: {
         email,
       },
@@ -25,20 +23,17 @@ $(document).ready(function() {
       },
       success: function(response) {
         if (response.length) {
-          $('.regMessage').html('User already exist');
+          $('.regMessage').html('User with the same email already exist!!');
         } else {
           //Submit the user data if the user does not exist
           $.ajax({
             method: 'POST',
-            url: 'http://localhost:3000/hotels',
+            url: 'http://localhost:3000/users',
             data: {
-              hotelname,
+              fullname,
+              username,
               email,
-              mobile,
-              website,
-              location,
-              city,
-              address,
+              password,
             },
             beforeSend: function() {
               $('.regMessage').html('Loading....');
@@ -47,11 +42,12 @@ $(document).ready(function() {
               $('.regMessage').html('Registration Successfull');
               localStorage.setItem('email', email);
               //if sign up successful
-              window.location.assign('index.html');
+              window.location.assign('login.html');
               return;
             },
           });
         }
       },
     });
-});
+  });
+})
