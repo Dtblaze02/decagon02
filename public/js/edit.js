6a89cb79678e485026d2a6993423963239280d8c
Dtblaze02 <sadiqaliyu89@gmail.com>
2019-09-19 20:03:23 +0100

    modified changes

---
+++ public/js/edit.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $.getJSON('http://localhost:3000/hotels', 
+    function(data){
+        $(data).each(function (i, hotels){
+            $('#hotelTable').append($("<tr>")
+            .append($("<td>").append(hotels.hotelname))
+            .append($("<td>").append(hotels.city))
+            .append($("<td>").append(hotels.location))
+            .append($("<td>").append(hotels.mobile))
+            .append($("<td>").append("<button  type = 'submit' style = 'color:white; background: green; padding:2px;'>Edit</button>"))
+            .append($("<td>").append("<button  type = 'submit' style = 'color:white; background: red; padding:2px;'>Delete</button>")))
+        });
+    });
+})
