13e2c06499d4d30fb76f40fb81b5f42933bc5b63
Dtblaze02 <sadiqaliyu89@gmail.com>
2019-09-20 08:33:03 +0100

    modified changes

--- public/js/edit.js
+++ public/js/edit.js
@@ -8,7 +8,22 @@ $(document).ready(function() {
             .append($("<td>").append(hotels.location))
             .append($("<td>").append(hotels.mobile))
             .append($("<td>").append("<button  type = 'submit' style = 'color:white; background: green; padding:2px;border-radius:10px;'>Edit</button>"))
-            .append($("<td>").append("<button  type = 'submit' style = 'color:white; background: red; padding:2px; border-radius:10px;'>Delete</button>")))
+            .append($("<td>").append("<button  type = 'submit' data-id ='{{id}}' class= 'remove' style = 'color:white; background: red; padding:2px; border-radius:10px;'>Delete</button>")))
         });
     });
-})
+});
+
+
+
+
+
+    $('#hotelTable').delegate('.remove','click', function(){
+
+    $.ajax({
+        type: 'DELETE',
+        url: 'http://localhost:3000/hotels' + $('#hotelTable').attr('data-id'),
+        success: function(){
+        
+        }
+    });
+});
